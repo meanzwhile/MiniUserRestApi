84e9533c06d949aa666e14807d4c497c043d7ffe
Peter Korossy <meanwhile@vipmail.hu>
2018-04-30 23:17:08 +0200

    Starter For js, css

---
+++ MiniUserRestApi/MiniUserRestApi/wwwroot/js/Main.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(main);
+
+function main() {
+    registerButtonClickListener();
+}
+
+function registerButtonClickListener() {
+    $("#registerButton").click(function () {
+        console.log("clicked");
+    });
+}
