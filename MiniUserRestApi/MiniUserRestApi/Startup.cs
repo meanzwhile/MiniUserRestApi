805af6fe442bd7f221f6228f823859474806f4bd
Peter Korossy <meanwhile@vipmail.hu>
2018-05-01 18:16:14 +0200

    InMemoryDatabase
    use this if cant connect to db

--- MiniUserRestApi/MiniUserRestApi/Startup.cs
+++ MiniUserRestApi/MiniUserRestApi/Startup.cs
@@ -1,42 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using MiniUserRestApi.Models;
+using MiniUserRestApi.Utilities;
 
 namespace MiniUserRestApi
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             var connection = @"Server=DESKTOP-2HGP57O\TESTINSTANCE;Database=MiniUser;Trusted_Connection=True;ConnectRetryCount=0";
-            try
+
+            if (Utils.CheckDbConnection(connection))
             {
                 services.AddDbContext<UserContext>(options => options.UseSqlServer(connection));
+
             }
-            catch (Exception)
+            else
             {
-                    
+                services.AddDbContext<UserContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
             }
-            //services.AddDbContext<UserContext>(options => options.UseInMemoryDatabase)
+
+
+
+            services.AddMvc();
         }
+
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
---
+++ MiniUserRestApi/MiniUserRestApi/Utilities/Check.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace MiniUserRestApi.Utilities
+{
+    public class Utils
+    {
+        public static bool CheckDbConnection(string connection)
+        {
+            using (var conn = new SqlConnection(connection))
+            {
+                try
+                {
+                    conn.Open();
+                    conn.Close();
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
