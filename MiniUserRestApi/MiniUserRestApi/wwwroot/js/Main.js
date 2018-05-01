4383a64db712b9f1fcb86a7ced7d92a499745cf8
Peter Korossy <meanwhile@vipmail.hu>
2018-05-01 14:34:47 +0200

    Basic functions Implemented
    Delete user, show all user, register new user

--- MiniUserRestApi/MiniUserRestApi/wwwroot/js/Main.js
+++ MiniUserRestApi/MiniUserRestApi/wwwroot/js/Main.js
@@ -1,11 +1,106 @@
 ﻿$(document).ready(main);
 
 function main() {
+    getAllUser();
     registerButtonClickListener();
+    registerUserClickListener();
 }
 
 function registerButtonClickListener() {
     $("#registerButton").click(function () {
-        console.log("clicked");
+        $("#allUser").hide();
+        $("#registerPage").show();
+    });
+}
+
+function registerUserClickListener() {
+    $("#register").click(function () {
+        var username = $("#username").val();
+        var email = $("#email").val();
+
+        var data = {
+            UserName: username,
+            UserEmail: email
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/User',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data),
+            success: function () {
+                var alertBox = $("#alertSuccess");
+                alertBox.fadeIn();
+                alertBox.text("Successfully registered the new user");
+                closeAlertBox(alertBox);
+                $("#allUser").show();
+                $("#registerPage").hide();
+                getAllUser();
+            },
+            error: function () {
+                var alertBox = $("#alertFail");
+                alertBox.fadeIn();
+                alertBox.text("Something went wrong :(");
+                closeAlertBox(alertBox);
+            }
+        })
+    })
+}
+
+function closeAlertBox(alertBox) {
+    window.setTimeout(function () {
+        alertBox.fadeOut(300)
+    }, 3000);
+}
+
+function getAllUser() {
+    $.ajax({
+        type: 'GET',
+        url: '/api/User',
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        success: function (result) {
+            $("#userTable tbody tr").remove();
+            for (i in result) {
+                $("#userTable").append("<tr>" +
+                "<td>" + result[i].id + "</td>" +
+                "<td>" + result[i].userName + "</td>" +
+                "<td>" + result[i].userEmail + "</td>" +
+                "<td><button id='" + result[i].id + "' class='btn btn-danger deleteButton'>X</button></td>" +
+                "</tr > ");
+
+            }
+            deleteUserButtonHandler();
+        },
+        error: function () {
+            var alertBox = $("#alertFail");
+            alertBox.fadeIn();
+            alertBox.text("Something went terribly wrong :(");
+            closeAlertBox(alertBox);
+        }
+    })
+}
+function deleteUserButtonHandler() {
+    $(".deleteButton").click(function () {
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/User/' + this.id,
+            success: function (result) {
+                var alertBox = $("#alertSuccess");
+                alertBox.fadeIn();
+                alertBox.text("Successfully deleted the user");
+                closeAlertBox(alertBox);
+                getAllUser();
+            },
+            error: function () {
+                var alertBox = $("#alertFail");
+                alertBox.fadeIn();
+                alertBox.text("Something went wrong :(");
+                closeAlertBox(alertBox);
+            }
+        })
+
+        getAllUser();
     });
 }
